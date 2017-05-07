ec8f0beb5ede58e89d34ad9e279f8df238ca1d56
wc42888 <z3184703@zmail.unsw.edu.au>
2017-05-08 08:47:56 +1000

    closure

--- es6/app/index.js
+++ es6/app/index.js
@@ -1,17 +1,10 @@
-function Wizard(name, house, pet) {
-    this.name = name;
-    this.house = house;
-    this.pet = pet;
-    
-    this.greet = () =>{
-        return `I am ${this.name} from ${this.house}`;
+let call = () => {
+    let secret = 'ES6 rocks!';
+    let reveal = () =>{
+        console.log(secret);
     }
+    return reveal;
 }
 
-Wizard.prototype.petName;
-Wizard.prototype.info = function() {
-    return `i have a ${this.pet} named ${this.petName}`
-};
-let harry = new Wizard("Harry potter", "gryggindor", "owl");
-harry.petName = "Hedwig";
-console.log(harry.info());
+let unveil = call();
+unveil();
